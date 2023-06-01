87485c9c089f6388a403f36cfb09a043c1d3ddc0
Zakir Bin Alam <77552607+ZIAN786@users.noreply.github.com>
2023-06-01 19:55:03 +0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,7 +58,7 @@ export default function Home() {
     return (
      <div>
         <Jumbotron 
-        title="E-Commerce" subTitle="Welcome To The Fashion Web"/>
+        title="Ecommerce" subTitle="Welcome To The Fashion Web"/>
 
         <div className="row">
           <div className="col-md-6">
